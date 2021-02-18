844c881ff9d1e4094461b9a67465f64435b68326
segovelo <segovelos@gmail.com>
2021-02-18 19:02:00 +0000

    Initial commit

---
+++ src/main/java/payroll/OrderNotFoundException.java
@@ -0,0 +1,13 @@
+package payroll;
+
+class OrderNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	OrderNotFoundException(Long id) {
+		super("Could not find order " + id);
+	}
+}
